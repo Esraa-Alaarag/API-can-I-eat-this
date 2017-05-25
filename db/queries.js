c07649ca62690695295f3b08952fdf2d1ac1fa8c
Esraa Alaarag <m_israa1981@hotmail.com>
2017-05-25 11:32:06 -0400

    patch is not going to work

--- db/queries.js
+++ db/queries.js
@@ -99,19 +99,17 @@ function adduserpref(req, res, next) {
 
 function addoredit(req, res, next) {
   console.log(req);
-  console.log('req.body ===>', req.body)
-   db.none('insert into allergies (userid ,eggsallergy , fishallergy , milkallergy, peanutsallergy, sesameallergy, shellfishallergy , soyallergy , treenutsallergy,wheatallergy)' +
-      'values(${userid} , ${eggsallergy} , ${fishallergy} , ${milkallergy},${peanutsallergy},${sesameallergy} ,${shellfishallergy},${soyallergy},${treenutsallergy},${wheatallergy}  )',
-      req.body)
+  db.none('update allergies set eggsallergy=$1, fishallergy=$2, milkallergy=$3, peanutsallergy=$4, sesameallergy=$5 ,shellfishallergy=$6 , soyallergy=$7,  treenutsallergy=$8 , wheatallergy=$9  where userid=$10', [req.body.eggsallergy, req.body.fishallergy, req.body.milkallergy, req.body.peanutsallergy, req.body.sesameallergy, req.body.shellfishallergy, req.body.soyallergy, req.body.treenutsallergy, req.body.wheatallergy , req.params.userid
+    ])
     .then(function() {
       res.status(200)
         .json({
           status: 'success',
-          message: 'One user preference was added'
+          message: ' one user preference was Updated'
         });
     })
     .catch(function(err) {
-      console.log("This user already exist in the database");
+      console.log(err)
       return next(err);
     });
 }
@@ -216,7 +214,7 @@ module.exports = {
   history      : history,        //read
   deleteproduct: deleteproduct,   //DELETE
   allusers     : allusers,        //read
-  addoredit    : addoredit,
+  addoredit    : addoredit
 };
 
 
